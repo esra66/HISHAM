fc25c9b245d7c67f9aab91759000d7eac0036cc6
Zqhisham <Zqhisham@gmail.com>
2023-10-12 11:12:49 +0400

    push to original

--- shell_main.c
+++ shell_main.c
@@ -9,33 +9,9 @@
 int main(int ac, char **av)
 {
 	info_t info[] = { INIT_INFO };
-	int f_desc = 2;
 
-	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (f_desc)
-			: "r" (f_desc));
+	(void)ac;
 
-	if (ac == 2)
-	{
-		f_desc = open(av[1], O_RDONLY);
-		if (f_desc == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can not open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_CLEAR);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->input_fd = f_desc;
-	}
 	pop_env_list(info);
 	re_history(info);
 	hsh(info, av);
